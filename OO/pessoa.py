6f077c3ea3506e7e48bad1ca97d3c17d1b91c978
Lucas Silva <lucasdafon.silva@hotmail.com>
2020-08-27 00:03:01 -0300

    Criado atributos de objeto "nome" e "idade".

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,10 +1,20 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Lucas', 28)
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
+    p.nome = 'Juca'
+    p.idade = 80
+    print(p.nome)
+    print(p.idade)
