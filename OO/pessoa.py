34356ca94bd017b0ca42532be6ca12e58ea94e3d
Lucas Silva <lucasdafon.silva@hotmail.com>
2020-08-27 02:15:31 -0300

    Criado atributo complexo "filhos".

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,20 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    def __init__(self, *filhos, nome=None, idade=None):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Lucas', 28)
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    print(p.idade)
-    p.nome = 'Juca'
-    p.idade = 80
-    print(p.nome)
-    print(p.idade)
+    lucas = Pessoa(nome='Lucas', idade=28)
+    carlos = Pessoa(lucas, nome='Carlos', idade=62)
+    print(Pessoa.cumprimentar(carlos))
+    print(id(carlos))
+    print(carlos.cumprimentar())
+    print(carlos.nome)
+    print(carlos.idade)
+    for filho in carlos.filhos:
+        print(filho.nome)
