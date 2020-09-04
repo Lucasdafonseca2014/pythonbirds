83809c55096f3cdc3f5b7a6866a8d866b2ff4b2d
Lucas Silva <lucasdafon.silva@hotmail.com>
2020-09-04 15:39:48 -0300

    Adicionando doctest às classes "Carro"

--- OO/carro_2/carro.py
+++ OO/carro_2/carro.py
@@ -3,6 +3,38 @@ from OO.carro_2.volante import Volante
 
 
 class Carro:
+    """
+        Classe que implementa a experiência de um carro, possuindo um motor e um volante.
+
+        * Atributos de dado: Modelo, cor, motor, volante.
+        * Métodos: Acelerar, frear, virar à direita, virar à esquerda, mostra velocidade, mostra direção.
+
+
+        DOCTEST: Amostra de como a classe "Carro" deve funcionar ao ser implementada.
+
+        >>> gol = Carro(modelo='Gol', cor='Prata')
+        >>> gol.modelo
+        'Gol'
+        >>> gol.cor
+        'Prata'
+        >>> gol.direcao()
+        'norte'
+        >>> gol.direita()
+        >>> gol.direcao()
+        'leste'
+        >>> gol.velocidade()
+        0
+        >>> gol.acelerar()
+        >>> gol.acelerar()
+        >>> gol.acelerar()
+        >>> gol.acelerar()
+        >>> gol.velocidade()
+        4
+        >>> gol.frear()
+        >>> gol.velocidade()
+        2
+
+        """
     def __init__(self, modelo='Gol', cor='Prata'):
         self.cor = cor
         self.modelo = modelo
